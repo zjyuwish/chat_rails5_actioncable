717a6998fdcac7368b22d5fa9e665cb265d5e60c
Jinyu Zhang <zjyuwish@hotmail.com>
2017-03-10 18:15:49 +0800

    init

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'chat_rooms#index'
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  mount ActionCable.server => '/cable'
+
+end
